76eeaf620ed7d13de17b0ca75fcb85be39895756
Trinethr N <trinethr_n@protonmail.com>
2023-01-19 22:10:43 +0530

    working typing test

--- main.js
+++ main.js
@@ -1,23 +1,142 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let allWords = [
+  "the",
+  "of",
+  "and",
+  "to",
+  "in",
+  "is",
+  "be",
+  "that",
+  "was",
+  "he",
+  "for",
+  "it",
+  "with",
+  "as",
+  "his",
+  "on",
+  "have",
+  "at",
+  "by",
+  "not",
+  "they",
+  "this",
+  "had",
+  "are",
+  "but",
+  "from",
+  "or",
+  "which",
+  "one",
+  "you",
+  "would",
+  "all",
+  "will",
+  "there",
+  "when",
+  "up",
+  "what",
+  "out",
+  "about",
+  "who",
+  "get",
+  "if",
+  "can",
+  "my",
+  "would",
+  "her",
+  "say",
+  "an",
+  "we",
+  "home",
+  "like",
+  "other",
+  "how",
+  "then",
+  "them",
+  "these",
+  "so",
+  "some",
+  "her",
+  "make",
+  "him",
+  "into",
+  "time",
+  "has",
+  "look",
+  "two",
+  "more",
+  "write",
+  "go",
+  "see",
+  "number",
+  "way",
+  "could",
+  "people",
+  "than"
+]
 
-setupCounter(document.querySelector('#counter'))
+let testWords = allWords.sort(() => 0.5 - Math.random())
+let testElement = document.querySelector("#testtext")
+let testString = testWords.join(' ')
+testElement.innerHTML = testString
+testElement.onclick = () => { testInput.focus() }
+let testStarted = false
+let testInput = document.getElementById("testinput")
+let countdown = document.getElementById("countdown")
+let userString = ''
+let scoreModal = document.getElementById("score")
+
+function startTest () {
+  testStarted = true
+  testElement.style.filter = "none"
+  countdown.innerHTML = "00:15"
+  testInput.focus()
+  testInput.value = ""
+  testInput.addEventListener('input', inputChanged)
+}
+
+function inputChanged (e) {
+  if (e.target.value.charAt(0) == e.target.value) {
+    startTimer()
+  }
+  userString = e.target.value
+}
+
+function startTimer() {
+  let secondsleft = 14
+  let timer = setInterval(() => {
+    secondsleft -= 1
+    countdown.innerHTML = "00:" + (secondsleft < 9 ? "0" : "") + (secondsleft + 1)
+    if (secondsleft == 0) {
+      clearInterval(timer)
+      stopTest()
+    }
+  }, 1000)
+}
+
+function stopTest () {
+  testInput.readOnly = true
+  let userWords = userString.split(' ')
+  let count = 0
+  userWords.forEach((word, index) => {
+    if (word == allWords[index]) {
+      count++
+    }
+  });
+  let wpm = count * 4
+  let wps = (wpm/60).toFixed(2)
+  let cps = (userString.length/60).toFixed(2)
+  showScore(wpm, wps, cps)
+}
+
+function showScore(wpm, wps, cps) {
+  document.getElementById("wpmdisplay").innerHTML = wpm
+  document.getElementById("wpsdisplay").innerHTML = `<span class="green">${wps}</span> word${wps > 1 ? "s" : ""} per second`
+  document.getElementById("cpsdisplay").innerHTML = `<span class="green">${cps}</span> character${wps > 1 ? "s" : ""} per second`
+  scoreModal.showModal()
+}
+
+document.getElementById("startbtn").onclick = startTest
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     outDir: 'docs'
-  }
+  },
+  base: '/typing-test/'
 })
